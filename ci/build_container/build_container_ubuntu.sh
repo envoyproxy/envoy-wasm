62d70b228c4f4969bd868ced1b34b9d48873d82d
Piotr Sikora <piotrsikora@google.com>
2018-10-24 19:50:48 +0000

    build: update CMake to version 3.12.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/build_container/build_container_ubuntu.sh
+++ ci/build_container/build_container_ubuntu.sh
@@ -5,15 +5,19 @@ set -e
 # Setup basic requirements and install them.
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y wget software-properties-common make cmake git python python-pip \
+apt-get install -y curl wget software-properties-common make git python python-pip \
   bc libtool ninja-build automake zip time golang g++ gdb strace wireshark tshark tcpdump
+# Install cmake 3.12.
+curl -sLO https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.tar.gz
+echo "0210f500c71af0ee7e8c42da76954298144d5f72f725ea381ae5db7b766b000e cmake-3.12.3-Linux-x86_64.tar.gz" | sha256sum --check
+tar -zxf cmake-3.12.3-Linux-x86_64.tar.gz -C /usr --strip-components=1
 # clang 7.
-wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+curl http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main"
 apt-get update
 apt-get install -y clang-7 clang-format-7 clang-tidy-7 lld-7
 # Bazel and related dependencies.
-apt-get install -y openjdk-8-jdk curl
+apt-get install -y openjdk-8-jdk
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
 apt-get update
