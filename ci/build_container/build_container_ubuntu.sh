bde25c1dcb2e21552d8b76da5cfcf493447147d8
Piotr Sikora <piotrsikora@google.com>
2019-05-10 13:54:06 -0700

    build: update ninja to version 1.8.2. (#65)
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/build_container/build_container_ubuntu.sh
+++ ci/build_container/build_container_ubuntu.sh
@@ -6,11 +6,15 @@ set -e
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y curl wget software-properties-common make cmake git python python-pip python3 python3-pip \
-  unzip bc libtool ninja-build automake zip time golang gdb strace wireshark tshark tcpdump
+  unzip bc libtool automake zip time golang gdb strace wireshark tshark tcpdump
 # Install cmake 3.12.
 curl -sLO https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.tar.gz
 echo "0210f500c71af0ee7e8c42da76954298144d5f72f725ea381ae5db7b766b000e cmake-3.12.3-Linux-x86_64.tar.gz" | sha256sum --check
 tar -zxf cmake-3.12.3-Linux-x86_64.tar.gz -C /usr --strip-components=1
+# Install ninja 1.8.2.
+curl -sLO https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+echo "d2fea9ff33b3ef353161ed906f260d565ca55b8ca0568fa07b1d2cab90a84a07 ninja-linux.zip" | sha256sum --check
+unzip ninja-linux.zip && mv ninja /usr/bin
 # clang 7.
 curl http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main"
