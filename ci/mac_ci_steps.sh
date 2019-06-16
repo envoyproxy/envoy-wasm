00c40820136d9282f335ed95fc1de18b7d8a0e70
Piotr Sikora <piotrsikora@google.com>
2019-06-16 02:58:32 +0000

    Revert "review: avoid invalidating cache between build and tests."
    This reverts commit 28decd956ed11d2e7bf953c8f3a8b0966157f149.
    
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/mac_ci_steps.sh
+++ ci/mac_ci_steps.sh
@@ -13,12 +13,12 @@ df -h
 
 . "$(dirname "$0")"/setup_gcs_cache.sh
 
-# TODO(zuercher): remove --flaky_test_attempts when https://github.com/envoyproxy/envoy/issues/2428
-# is resolved.
 BAZEL_BUILD_OPTIONS="--curses=no --show_task_finish --verbose_failures ${BAZEL_BUILD_EXTRA_OPTIONS} \
   --deleted_packages //test/extensions/quic_listeners/quiche/platform \
-  --action_env=PATH=/usr/local/bin:/opt/local/bin:/usr/bin:/bin \
-  --test_output=all --flaky_test_attempts=integration@2"
+  --action_env=PATH=/usr/local/bin:/opt/local/bin:/usr/bin:/bin"
+# TODO(zuercher): remove --flaky_test_attempts when https://github.com/envoyproxy/envoy/issues/2428
+# is resolved.
+BAZEL_TEST_OPTIONS="${BAZEL_BUILD_OPTIONS} --test_output=all --flaky_test_attempts=integration@2"
 
 # Build envoy and run tests as separate steps so that failure output
 # is somewhat more deterministic (rather than interleaving the build
@@ -31,4 +31,4 @@ else
 fi
 
 bazel build ${BAZEL_BUILD_OPTIONS} //source/exe:envoy-static ${TEST_TARGETS}
-bazel test ${BAZEL_BUILD_OPTIONS} ${TEST_TARGETS}
+bazel test ${BAZEL_TEST_OPTIONS} ${TEST_TARGETS}
