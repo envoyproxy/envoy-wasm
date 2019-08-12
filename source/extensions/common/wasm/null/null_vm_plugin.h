84324fa15e5ccc17c9f7574c2d6b1e706d88d424
John Plevyak <jplevyak@gmail.com>
2019-08-12 13:56:42 -0700

    Address comments.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- source/extensions/common/wasm/null/null_vm_plugin.h
+++ source/extensions/common/wasm/null/null_vm_plugin.h
@@ -13,17 +13,17 @@ public:
   NullVmPlugin() {}
   virtual ~NullVmPlugin() {}
 
-  virtual void getFunction(absl::string_view functionName, WasmCall0Void* f) = 0;
-  virtual void getFunction(absl::string_view functionName, WasmCall1Void* f) = 0;
-  virtual void getFunction(absl::string_view functionName, WasmCall2Void* f) = 0;
-  virtual void getFunction(absl::string_view functionName, WasmCall3Void* f) = 0;
-  virtual void getFunction(absl::string_view functionName, WasmCall4Void* f) = 0;
-  virtual void getFunction(absl::string_view functionName, WasmCall5Void* f) = 0;
-  virtual void getFunction(absl::string_view functionName, WasmCall8Void* f) = 0;
-  virtual void getFunction(absl::string_view functionName, WasmCall1Word* f) = 0;
-  virtual void getFunction(absl::string_view functionName, WasmCall3Word* f) = 0;
+  virtual void getFunction(absl::string_view functionName, WasmCall0Void* f) PURE;
+  virtual void getFunction(absl::string_view functionName, WasmCall1Void* f) PURE;
+  virtual void getFunction(absl::string_view functionName, WasmCall2Void* f) PURE;
+  virtual void getFunction(absl::string_view functionName, WasmCall3Void* f) PURE;
+  virtual void getFunction(absl::string_view functionName, WasmCall4Void* f) PURE;
+  virtual void getFunction(absl::string_view functionName, WasmCall5Void* f) PURE;
+  virtual void getFunction(absl::string_view functionName, WasmCall8Void* f) PURE;
+  virtual void getFunction(absl::string_view functionName, WasmCall1Word* f) PURE;
+  virtual void getFunction(absl::string_view functionName, WasmCall3Word* f) PURE;
 
-  virtual void start() = 0;
+  virtual void start() PURE;
 };
 
 /**
