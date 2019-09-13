273c3b456bc5a1c4ab4981cb83e7957c04c40656
John Plevyak <jplevyak@gmail.com>
2019-09-13 12:28:38 -0700

    Fix tests.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- test/extensions/wasm/test_data/bad_signature_cpp.cc
+++ test/extensions/wasm/test_data/bad_signature_cpp.cc
@@ -3,9 +3,10 @@
 
 #include "proxy_wasm_intrinsics.h"
 
-extern "C" EMSCRIPTEN_KEEPALIVE void proxy_onConfigure(uint32_t, int bad, char* configuration,
-                                                       int size) {
+extern "C" EMSCRIPTEN_KEEPALIVE uint32_t proxy_onConfigure(uint32_t, int bad, char* configuration,
+                                                           int size) {
   logWarn(std::string("warn " + std::string(configuration, size)));
+  return 1;
 }
 
 extern "C" EMSCRIPTEN_KEEPALIVE void proxy_onStart(uint32_t, uint32_t, uint32_t, uint32_t,
