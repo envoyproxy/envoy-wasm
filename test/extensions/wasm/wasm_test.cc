6a820a99ebf2aa548e6800b24e685f6106e8d061
Piotr Sikora <piotrsikora@google.com>
2019-10-19 02:08:03 +0000

    review: re-add SIGSEGV test and check for "unreachable".
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- test/extensions/wasm/wasm_test.cc
+++ test/extensions/wasm/wasm_test.cc
@@ -123,6 +123,43 @@ TEST_P(WasmTest, BadSignature) {
                             "Bad function signature for: proxy_onConfigure");
 }
 
+TEST_P(WasmTest, Segv) {
+  Stats::IsolatedStoreImpl stats_store;
+  Api::ApiPtr api = Api::createApiForTest(stats_store);
+  Upstream::MockClusterManager cluster_manager;
+  Event::DispatcherPtr dispatcher(api->allocateDispatcher());
+  auto scope = Stats::ScopeSharedPtr(stats_store.createScope("wasm."));
+  NiceMock<LocalInfo::MockLocalInfo> local_info;
+  auto name = "";
+  auto root_id = "";
+  auto vm_id = "";
+  auto vm_configuration = "";
+  auto plugin = std::make_shared<Extensions::Common::Wasm::Plugin>(
+      name, root_id, vm_id, envoy::api::v2::core::TrafficDirection::UNSPECIFIED, local_info,
+      nullptr, *scope, scope);
+  auto wasm = std::make_unique<Extensions::Common::Wasm::Wasm>(
+      absl::StrCat("envoy.wasm.runtime.", GetParam()), vm_id, vm_configuration, plugin,
+      cluster_manager, *dispatcher);
+  const auto code = TestEnvironment::readFileToStringForTest(TestEnvironment::substitute(
+      "{{ test_rundir }}/test/extensions/wasm/test_data/segv_cpp.wasm"));
+  EXPECT_FALSE(code.empty());
+  auto context = std::make_unique<TestContext>(wasm.get());
+  EXPECT_CALL(*context, scriptLog_(spdlog::level::err, Eq("before badptr")));
+  EXPECT_TRUE(wasm->initialize(code, false));
+
+  if (GetParam() == "v8") {
+    EXPECT_THROW_WITH_MESSAGE(wasm->startForTesting(std::move(context)),
+                              Extensions::Common::Wasm::WasmException,
+                              "Function: proxy_onStart failed: Uncaught RuntimeError: unreachable");
+  } else if (GetParam() == "wavm") {
+    EXPECT_THROW_WITH_REGEX(wasm->startForTesting(std::move(context)),
+                            Extensions::Common::Wasm::WasmException,
+                            "Function: proxy_onStart failed: wavm.reachedUnreachable.*");
+  } else {
+    ASSERT_FALSE(true); // Neither of the above was matched.
+  }
+}
+
 TEST_P(WasmTest, DivByZero) {
   Stats::IsolatedStoreImpl stats_store;
   Api::ApiPtr api = Api::createApiForTest(stats_store);
