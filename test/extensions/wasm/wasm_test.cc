99c0ab210de57eeaca1a5a1c14487bb6adfe0991
John Plevyak <jplevyak@gmail.com>
2019-11-18 17:43:26 +0000

    Use weak_ptr to check for liveness.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- test/extensions/wasm/wasm_test.cc
+++ test/extensions/wasm/wasm_test.cc
@@ -73,17 +73,17 @@ TEST_P(WasmTestMatrix, Logging) {
   auto plugin = std::make_shared<Extensions::Common::Wasm::Plugin>(
       name, root_id, vm_id, envoy::api::v2::core::TrafficDirection::UNSPECIFIED, local_info,
       nullptr);
-  auto wasm = std::make_unique<Extensions::Common::Wasm::Wasm>(
+  auto wasm = std::make_shared<Extensions::Common::Wasm::Wasm>(
       absl::StrCat("envoy.wasm.runtime.", std::get<0>(GetParam())), vm_id, vm_configuration, scope,
       cluster_manager, *dispatcher);
   EXPECT_NE(wasm, nullptr);
-  auto wasm_ptr = wasm.get();
+  auto wasm_weak = std::weak_ptr<Extensions::Common::Wasm::Wasm>(wasm);
   auto wasm_handler = std::make_unique<Extensions::Common::Wasm::WasmHandle>(std::move(wasm));
   const auto code = TestEnvironment::readFileToStringForTest(TestEnvironment::substitute(
       absl::StrCat("{{ test_rundir }}/test/extensions/wasm/test_data/logging_",
                    std::get<1>(GetParam()), ".wasm")));
   EXPECT_FALSE(code.empty());
-  auto context = std::make_unique<TestContext>(wasm_ptr);
+  auto context = std::make_unique<TestContext>(wasm_weak.lock().get());
 
   EXPECT_CALL(*context, scriptLog_(spdlog::level::warn, Eq("warn configure-test")));
   EXPECT_CALL(*context, scriptLog_(spdlog::level::trace, Eq("test trace logging")));
@@ -93,14 +93,15 @@ TEST_P(WasmTestMatrix, Logging) {
   EXPECT_CALL(*context, scriptLog_(spdlog::level::info, Eq("onDone logging")));
   EXPECT_CALL(*context, scriptLog_(spdlog::level::info, Eq("onDelete logging")));
 
-  EXPECT_TRUE(wasm_ptr->initialize(code, false));
-  wasm_ptr->setContext(context.get());
+  EXPECT_TRUE(wasm_weak.lock()->initialize(code, false));
+  wasm_weak.lock()->setContext(context.get());
   auto root_context = context.get();
-  wasm_ptr->startForTesting(std::move(context), plugin);
-  wasm_ptr->configure(root_context, "configure-test");
+  wasm_weak.lock()->startForTesting(std::move(context), plugin);
+  wasm_weak.lock()->configure(root_context, "configure-test");
   wasm_handler.reset();
   dispatcher->run(Event::Dispatcher::RunType::NonBlock);
-  wasm_ptr->tickHandler(root_context->id());
+  // This will SEGV on nullptr if wasm has been deleeted.
+  wasm_weak.lock()->tickHandler(root_context->id());
   dispatcher->run(Event::Dispatcher::RunType::NonBlock);
   dispatcher->clearDeferredDeleteList();
 }
