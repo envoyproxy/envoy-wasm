2de976adb0eb97eb7eeccbb0112f4eea5a554ea5
John Plevyak <jplevyak@gmail.com>
2019-12-05 22:27:35 +0000

    Address comments.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- test/extensions/wasm/test_data/bad_signature_cpp.cc
+++ test/extensions/wasm/test_data/bad_signature_cpp.cc
@@ -1,11 +1,11 @@
 // NOLINT(namespace-envoy)
 #include <string>
 
-#define EMSCRIPTEN_KEEPALIVE __attribute__((used)) __attribute__((visibility("default")))
+#define PROXY_WASM_KEEPALIVE __attribute__((used)) __attribute__((visibility("default")))
 
 extern "C" uint32_t proxy_log(uint32_t level, const char* logMessage, size_t messageSize);
 
-extern "C" EMSCRIPTEN_KEEPALIVE uint32_t proxy_on_configure(uint32_t, int bad, char* configuration,
+extern "C" PROXY_WASM_KEEPALIVE uint32_t proxy_on_configure(uint32_t, int bad, char* configuration,
                                                             int size) {
   std::string message = "bad signature";
   proxy_log(4 /* error */, message.c_str(), message.size());
