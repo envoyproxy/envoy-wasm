7b74ccaf5c02ff12994c68ee6bfd9cc94a984851
John Plevyak <jplevyak@gmail.com>
2020-05-18 10:18:07 -0700

    Add test for full coverage of extensions/wasm/config.cc and fix issue. (#518)
    * Add test for full coverage of extensions/wasm/config.cc and fix issue
    it detected.`
    
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- test/extensions/wasm/test_data/start_cpp.cc
+++ test/extensions/wasm/test_data/start_cpp.cc
@@ -10,3 +10,8 @@ extern "C" PROXY_WASM_KEEPALIVE uint32_t proxy_on_vm_start(uint32_t, uint32_t) {
   logDebug("onStart");
   return 1;
 }
+
+extern "C" PROXY_WASM_KEEPALIVE uint32_t proxy_on_configure(uint32_t, uint32_t configuration_size) {
+  // Fail if we are provided a non-empty configuration.
+  return configuration_size ? 0 /* failure */ : 1 /* success */;
+}
