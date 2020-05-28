b1553f449a9a3b36424492330bded098cf419efa
John Plevyak <jplevyak@gmail.com>
2020-05-28 14:15:19 -0700

    Add comments.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- source/extensions/common/wasm/ext/envoy_null_vm_wasm_api.h
+++ source/extensions/common/wasm/ext/envoy_null_vm_wasm_api.h
@@ -11,12 +11,17 @@ using StringView = absl::string_view;
 #include "proxy_wasm_enums.h"
 #include "proxy_wasm_externs.h"
 
-#define _THE_FOLLOWING_FILE_MUST_COME_AFTER_THOSE_ABOVE_ 1
+/*
+ * The following headers are used in two different environments, in the Null VM and in Wasm code
+ * which require different headers to preceed these  such that they can not include the above
+ * headers directly. These macros prevent header reordering
+ */
+#define _THE_FOLLOWING_INCLUDE_MUST_COME_AFTER_THOSE_ABOVE_ 1
 #include "proxy_wasm_api.h"
-#undef _THE_FOLLOWING_FILE_MUST_COME_AFTER_THOSE_ABOVE_
-#define _THE_FOLLOWING_FILE_MUST_COME_AFTER_THOSE_ABOVE_ 1
+#undef _THE_FOLLOWING_INCLUDE_MUST_COME_AFTER_THOSE_ABOVE_
+#define _THE_FOLLOWING_INCLUDE_MUST_COME_AFTER_THOSE_ABOVE_ 1
 #include "extensions/common/wasm/ext/envoy_proxy_wasm_api.h"
-#undef _THE_FOLLOWING_FILE_MUST_COME_AFTER_THOSE_ABOVE_
+#undef _THE_FOLLOWING_INCLUDE_MUST_COME_AFTER_THOSE_ABOVE_
 
 } // namespace null_plugin
 } // namespace proxy_wasm
