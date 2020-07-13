271f2423472db2037002adb6316b1f45d0a763d6
John Plevyak <jplevyak@gmail.com>
2020-07-13 17:41:27 +0000

    Add tests for stderr stdout.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- test/extensions/bootstrap/wasm/test_data/logging_cpp.cc
+++ test/extensions/bootstrap/wasm/test_data/logging_cpp.cc
@@ -1,5 +1,6 @@
 // NOLINT(namespace-envoy)
 #include <string>
+#include <stdio.h>
 
 #include "proxy_wasm_intrinsics.h"
 
@@ -7,6 +8,9 @@
 extern "C" PROXY_WASM_KEEPALIVE void proxy_abi_version_0_1_0() {}
 
 extern "C" PROXY_WASM_KEEPALIVE uint32_t proxy_on_configure(uint32_t, uint32_t configuration_size) {
+  fprintf(stdout, "printf stdout test");
+  fflush(stdout);
+  fprintf(stderr, "printf stderr test");
   logTrace("test trace logging");
   logDebug("test debug logging");
   logError("test error logging");
