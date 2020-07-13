a7dc6300b62d3187d7685e595888e6a5bc42229a
John Plevyak <jplevyak@gmail.com>
2020-07-13 11:23:22 -0700

    Format.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- source/extensions/common/wasm/ext/envoy_null_plugin.h
+++ source/extensions/common/wasm/ext/envoy_null_plugin.h
@@ -5,8 +5,10 @@
 #define PROXY_WASM_PROTOBUF_FULL 1
 
 #include "envoy/config/core/v3/grpc_service.pb.h"
-#include "include/proxy-wasm/null_plugin.h"
+
 #include "source/extensions/common/wasm/ext/declare_property.pb.h"
+
+#include "include/proxy-wasm/null_plugin.h"
 
 namespace proxy_wasm {
 namespace null_plugin {
