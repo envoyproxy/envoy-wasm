5a4932132840d05f19a464590f59c1da540c84f0
Piotr Sikora <piotrsikora@google.com>
2020-08-05 02:50:55 +0000

    review: disable optimizations in DivByZero tests.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- test/extensions/bootstrap/wasm/test_data/segv_cpp.cc
+++ test/extensions/bootstrap/wasm/test_data/segv_cpp.cc
@@ -8,8 +8,6 @@ extern "C" PROXY_WASM_KEEPALIVE void proxy_abi_version_0_1_0() {}
 
 static int* badptr = nullptr;
 
-int will_be_zero = 0;
-
 extern "C" PROXY_WASM_KEEPALIVE uint32_t proxy_on_configure(uint32_t, uint32_t) {
   logError("before badptr");
   *badptr = 1;
@@ -17,8 +15,11 @@ extern "C" PROXY_WASM_KEEPALIVE uint32_t proxy_on_configure(uint32_t, uint32_t) 
   return 1;
 }
 
-extern "C" PROXY_WASM_KEEPALIVE void proxy_on_log(uint32_t) {
+extern "C" PROXY_WASM_KEEPALIVE void proxy_on_log(uint32_t context_id) {
   logError("before div by zero");
-  logError("divide by zero: " + std::to_string(100 / will_be_zero));
+#pragma clang optimize off
+  int zero = context_id / 1000;
+  logError("divide by zero: " + std::to_string(100 / zero));
+#pragma clang optimize on
   logError("after div by zero");
 }
